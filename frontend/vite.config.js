d3e6019cca13d6afd1f0e37ce6f2c5c45398c26b
Ketan Sahu <sahuketan305@gmail.com>
2025-11-23 03:52:15 +0530

    index.html not found fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: './frontend', // <- make sure root is set to frontend if your index.html is there
+  build: {
+    outDir: 'dist', // relative to root
+  },
   server:{
     proxy:{
       "/api":{
